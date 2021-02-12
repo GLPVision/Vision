15ebd387e05bbca46ffd171364cc9b89cd11cd06
Antoine <43754408+Klbgr@users.noreply.github.com>
2021-02-12 16:16:05 +0100

    Add files via upload

---
+++ src/data/test.java
@@ -0,0 +1,10 @@
+package data;
+
+import java.io.IOException;
+
+public class test {
+    public static void main(String args []) throws IOException {
+        new Otage(100, 100);
+        new Agriculture(100, 100);
+    }
+}
