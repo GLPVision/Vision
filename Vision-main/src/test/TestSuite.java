560f3ccde4a2768678dfe319f0caeb13b67b9496
Christian BERANGER <78799227+ROSChakalos@users.noreply.github.com>
2021-03-09 09:03:11 +0100

    Tout est dans src...

--- Vision-main/src/test/TestSuite.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import org.junit.runners.Suite;
-import org.junit.runner.RunWith;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({TestBuildOtage.class, TestBuildAgricole.class, TestTraitementOtage.class, TestTraitementAgricole.class})
-public class TestSuite {
-}
